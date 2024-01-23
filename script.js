efbf5e6dc3876f139858e4af2a28517c9cb6bd9b
trainEnthusiast314 <trainsAreCool@protonmail.com>
2024-01-23 13:58:45 +0000

    sorting js script more

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 // my-script.js
 document.addEventListener("DOMContentLoaded", function() {
     // this function runs when the DOM is ready, i.e. when the document has been parsed
-    document.getElementById("test").onclick=()=>{
-        document.getElementById("test").style.color = "#ff0000"
+    // let flag=true
+    // document.getElementById("test").onclick=(event)=>{
+    //     let flag=true
+    //     if (flag){
+    //         document.getElementById("test").style.color = "#ff0000"
+    //         flag=false
+    //     } else{
+    //         document.getElementById("test").style.color = "#000000"
+    //         flag=true
+    //     }
+    // }
+   const changeColorToRed=(event)=>{
+    
+    let elementId=event.target.attributes[0].value;
+    let currentColor=document.getElementById(elementId).style.color
+    
+    if(currentColor==="#ff0000"){
+        document.getElementById(elementId).style.color="#000000"
+    } else{
+        document.getElementById(elementId).style.color="#ff0000"
     }
-    changeColorToRed=(id)=>{
-        document.getElementById(id)
-    }
+   }
+   document.getElementById("test").onclick=changeColorToRed
 });

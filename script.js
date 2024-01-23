e67d9673e1fb7ce14655223c194c5f815c9dc1b6
trainEnthusiast314 <trainsAreCool@protonmail.com>
2024-01-23 13:25:18 +0000

    added functioning JS script to change main page title red

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // my-script.js
 document.addEventListener("DOMContentLoaded", function() {
     // this function runs when the DOM is ready, i.e. when the document has been parsed
-    document.getElementById("test").innerHTML = "Welcome back, Bart";
+    document.getElementById("test").onclick=()=>{
+        document.getElementById("test").style.color = "#ff0000"
+    }
+    changeColorToRed=(id)=>{
+        document.getElementById(id)
+    }
 });

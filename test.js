b501d1c7b9934e2494b83ba0d10af753c95aa2f7
trainEnthusiast314 <trainsAreCool@protonmail.com>
2024-01-26 16:23:51 +0000

    stuff

---
+++ test.js
@@ -0,0 +1,14 @@
+class Person {
+  name;
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  introduceSelf() {
+    console.log(`Hi! I'm ${this.name}`);
+  }
+}
+
+const dan = new Person("dan");
+console.log(dan.name);
